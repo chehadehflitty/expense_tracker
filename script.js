e31e4cd08b518d78e7b65574e8bded45eda3d62b
Chehadeh Flitty <flitty.chehadeh@gmail.com>
2023-07-19 09:02:27 +0300

    styling the page

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ function toDoItemElement(text,num){
   </tr>`
 }
 
+function addAmount(n){
+  let first_value=parseInt($("#amountinp").val());
+  return n+=first_value;
+}
+
 function addItem(){
   const expense_inp=$("#expenseinp");
   const amount_inp=$("#amountinp");
   const expense_tbl=$("#expensetbl");
+  var intamount=0
   if(expense_inp.val().trim()===""  || amount_inp.val().trim()==="") return;
 
   const to_do_item=$(toDoItemElement(expense_inp.val(), amount_inp.val()))
@@ -17,11 +23,15 @@ function addItem(){
     to_do_item.remove()
   })
   expense_tbl.append(to_do_item)
+  addAmount(intamount)
+  $("#amount").text(intamount)
   expense_inp.val("")
   amount_inp.val("")
+
 }
 
 $(document).ready(function(){
   const add_btn=$("#addbtn")
   add_btn.click(addItem)
+  
 })
